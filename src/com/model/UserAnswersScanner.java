00cfba07e21722d83fb8e6fd02f7287fe2fb1180
Dmytro Kozlovskyi <dmytrokozlovskyi@gmail.com>
2022-01-20 19:58:50 +0200

    Added class UserAnswersScanner for give method in class CashMachine

---
+++ src/com/model/UserAnswersScanner.java
@@ -0,0 +1,24 @@
+package com.model;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class UserAnswersScanner {
+    /**
+     * @param totalSumInMachine - How much is in {@link BanknotesCounter}
+     * @return boolean - User answer;
+     */
+    public boolean ifNotEnoughMoneyInMachine(int totalSumInMachine) {
+        System.out.println("Available sum to pay is: " + totalSumInMachine + ". Pay it?");
+        Scanner scan = new Scanner(System.in);
+        String answer = scan.nextLine().toUpperCase(Locale.ROOT);
+
+        if (answer.equals("Y")) {
+            System.out.println("You have confirmed payment of " + totalSumInMachine + " grn.");
+            return true;
+        } else {
+            System.out.println("Operation rejected.");
+            return false;
+        }
+    }
+}
