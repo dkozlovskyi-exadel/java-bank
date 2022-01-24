31b8a61dd84de8b701e63fbdc502ecd2351441ac
Dmytro Kozlovskyi <dmytrokozlovskyi@gmail.com>
2022-01-24 09:45:43 +0200

    Added collection with Account classes

---
+++ src/com/model/Account.java
@@ -0,0 +1,15 @@
+package com.model;
+
+public class Account {
+    public int userCash;
+
+    public Account(int initCash) {
+        this.userCash = initCash;
+    }
+
+    public void putCash() {
+    }
+
+    public void getCash() {
+    }
+}
---
+++ src/com/model/AccountsCollection.java
@@ -0,0 +1,29 @@
+package com.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class AccountsCollection {
+    private final Map<String, Account> activeUsersMap = new HashMap<>();
+
+    public Account getUserAccount(String incomeId) {
+        Account currentUserAccount;
+        if (activeUsersMap.containsKey(incomeId)) {
+            currentUserAccount = activeUsersMap.get(incomeId);
+        } else {
+            currentUserAccount = addNewUser();
+        }
+        return currentUserAccount;
+    }
+
+    private Account addNewUser() {
+        Account newUserAccount;
+        System.out.println("Need to sign up! Write your id one more time");
+        String newUserId = new Scanner(System.in).nextLine();
+
+        newUserAccount = new Account(1000);
+        activeUsersMap.put(newUserId, newUserAccount);
+        return newUserAccount;
+    }
+}
